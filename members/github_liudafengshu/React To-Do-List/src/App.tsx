a98dc3865043891731fe26c9de3bd42a44b934cd
liuyi1 <liuyi1@thinkive.com>
2024-06-04 19:34:05 +0800

    tesk 1

--- members/github_liudafengshu/React To-Do-List/src/App.tsx
+++ members/github_liudafengshu/React To-Do-List/src/App.tsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import PeddindComponent from "./componets/PeddindComponent.tsx";
+import CompletedListComponen from "./componets/CompletedListComponen.tsx";
+import Head from "./componets/Head.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [peddindList, setPenddingList] = useState<string[]>([]);
+  const [completedList, setCompletedList] = useState<string[]>([]);
+  const [inputValue, setInputeValue] = useState("");
+
+  useEffect(() => {
+    setPenddingList(JSON.parse(localStorage.getItem("peddindList") as string));
+    setCompletedList(
+      JSON.parse(localStorage.getItem("completedList") as string)
+    );
+  }, []);
+
+  window.onbeforeunload = function () {
+    localStorage.setItem("peddindList", JSON.stringify(peddindList));
+    localStorage.setItem("completedList", JSON.stringify(completedList));
+  };
+
+  function fulfillment(index: number) {
+    const str = peddindList.splice(index, 1);
+    setPenddingList([...peddindList]);
+    setCompletedList([...completedList, ...str]);
+  }
+  function removePedding(index: number) {
+    peddindList.splice(index, 1);
+    setPenddingList([...peddindList]);
+  }
+
+  function regression(index: number) {
+    const str = completedList.splice(index, 1);
+    setCompletedList([...completedList]);
+    setPenddingList([...peddindList, ...str]);
+  }
+
+  function removeCompleted(index: number) {
+    completedList.splice(index, 1);
+    setCompletedList([...completedList]);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{ width: "400px", margin: "0 auto" }}>
+        <Head />
+        <div>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => {
+              setInputeValue(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              setPenddingList([...peddindList, inputValue]);
+            }}
+          >
+            添加
+          </button>
+        </div>
+        <div>
+          <div>
+            <h2>待办</h2>
+            <PeddindComponent
+              peddindList={peddindList}
+              fulfillment={fulfillment}
+              removePedding={removePedding}
+            />
+          </div>
+          <div>
+            <h2>已办</h2>
+            <CompletedListComponen
+              completedList={completedList}
+              regression={regression}
+              removeCompleted={removeCompleted}
+            />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
