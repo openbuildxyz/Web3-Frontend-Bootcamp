15918ecf8fb6c9b2db3601267a2c412ef8f4e2d0
steak-zhuo <zhuoqinyue@gmail.com>
2024-06-05 02:24:08 +0800

    feat: update the font color

--- members/64182179/task1/src/components/footer.tsx
+++ members/64182179/task1/src/components/footer.tsx
@@ -6,8 +6,14 @@ interface FooterProps {
 }
 
 const Footer = ({ len, finishLen }: FooterProps) => (
-  <div style={{ display: "flex", flexDirection: "row-reverse", marginTop: 12 }}>
-    <Typography.Paragraph>
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "row-reverse",
+      marginTop: 12,
+    }}
+  >
+    <Typography.Paragraph style={{ color: "#1a3" }}>
       共計有 {len || 0} 條代辦, 你已完成 {finishLen} 條
     </Typography.Paragraph>
   </div>
