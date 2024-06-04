3889e2f611dd76127baef8e9e264c313d574fbc5
dreamahouse <dreamahourse@gmail.com>
2024-06-04 22:54:10 +0800

    报名+Task1

--- members/Beavnvvv/task1/readme.md
+++ members/Beavnvvv/task1/readme.md
@@ -1,1 +1,15 @@
+# Web3 前端训练营报名
 
+## 个人信息
+
+* Github ID: dreamahouse
+
+* Wechat ID: Dreamahouse
+
+* Openbuild Username: Ruby He
+
+(首次如果没有，学习完毕钱包在填写)
+
+* ERC20 钱包地址: 0xcB26196930F24a6847a58dBf5d80C6254d8De620
+
+### 个人介绍
---
+++ members/Beavnvvv/task1/src/components/ToDoList.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ToDoItem from './ToDoItem';
+import { ToDoItemType } from '../types';
+
+interface ToDoListProps {
+  todos: ToDoItemType[];
+  onDelete: (id: number) => void;
+  onToggleComplete: (id: number) => void;
+}
+
+const ToDoList: React.FC<ToDoListProps> = ({ todos, onDelete, onToggleComplete }) => {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <ToDoItem 
+          key={todo.id}
+          todo={todo}
+          onDelete={onDelete}
+          onToggleComplete={onToggleComplete}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ToDoList;
