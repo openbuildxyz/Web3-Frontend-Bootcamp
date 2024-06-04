f26be92697da301f0bc5d1a65eca878d81b3d0a1
zhouBoom <zb727904198@sina.com>
2024-06-05 00:46:06 +0800

    feat: todolist

---
+++ members/zhouBoom/taskTodoList/src/compents/ToDoItem.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+function ToDoItem({ todo, todos, setTodos, index }) {
+  const deleteTodo = () => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <li style={{ display: 'flex', alignItems: 'center' }}>
+      <span
+        style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+        onClick={() => {
+          const newTodos = [...todos];
+          newTodos[index].completed = !newTodos[index].completed;
+          setTodos(newTodos);
+        }}
+      >
+        {todo.text}
+      </span>
+      {/* <Button onClick={deleteTodo}> */}
+        <DeleteIcon style={{ marginLeft: '12px' }} onClick={deleteTodo}/>
+      {/* </Button> */}
+    </li>
+  );
+}
+
+export default ToDoItem;
