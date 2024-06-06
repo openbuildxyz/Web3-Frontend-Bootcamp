db0d312e296269ba5aed120adc8ffa27eaecdd1a
Jrenc <87536202+lulu546@users.noreply.github.com>
2024-06-06 20:49:51 +0800

    1. task1: lulu546

---
+++ members/lulu546/task1/postcss.config.mjs
@@ -0,0 +1,11 @@
+import autoprefixer from 'autoprefixer';
+
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}
+  },
+};
+
+export default config;
---
+++ members/lulu546/task1/tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui")
+  ],
+  darkMode: "selector"
+};
+export default config;
