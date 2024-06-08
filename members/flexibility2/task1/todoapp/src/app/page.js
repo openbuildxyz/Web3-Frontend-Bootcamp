48cc6eacdca8b735588af7ca6dd055c448d563b0
XWu32 <XWu32@slb.com>
2024-06-08 23:25:46 +0800

    add item & mark complete& edit item title

--- members/flexibility2/task1/todoapp/src/app/page.js
+++ members/flexibility2/task1/todoapp/src/app/page.js
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Header from "@/components/Header";
 import Form from "@/components/Form";
@@ -5,12 +6,24 @@ import TODOHero from "@/components/TODOHero";
 import TODOList from "@/components/TODOList";
 
 function Home() {
+  const [todos, setTodos] = React.useState([
+    // { title: "Some task", id: self.crypto.randomUUID(), is_completed: false },
+    // {
+    //   title: "Some other task",
+    //   id: self.crypto.randomUUID(),
+    //   is_completed: true,
+    // },
+    // { title: "last task", id: self.crypto.randomUUID(), is_completed: false },
+  ]);
+  const todos_completed =
+    todos?.filter((item) => item.is_completed === true).length ?? 0;
+  const total_todos = todos?.length ?? 0;
   return (
     <div className="wrapper">
       <Header />
-      <TODOHero todos_completed={0} total_todos={0} />
-      <Form />
-      <TODOList todos={[]} />
+      <TODOHero todos_completed={todos_completed} total_todos={total_todos} />
+      <Form setTodos={setTodos} />
+      <TODOList todos={todos} setTodos={setTodos} />
     </div>
   );
 }
--- members/flexibility2/task1/todoapp/src/components/Form.jsx
+++ members/flexibility2/task1/todoapp/src/components/Form.jsx
@@ -1,7 +1,13 @@
-"use client";
-const Form = () => {
+const Form = ({ setTodos }) => {
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    const value = event.target.todo.value;
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { title: value, id: self.crypto.randomUUID(), is_completed: false },
+    ]);
+
     event.target.reset();
   };
   return (
