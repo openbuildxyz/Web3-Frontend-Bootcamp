701990d9568388a3fed94582841d1e89d7296a71
Jerrold <1816801827@qq.com>
2024-06-08 15:45:54 +0800

    task1:STAJJJ

---
+++ members/STAJJJ/.history/task1/src/components/ToDoItem_20240608150935.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ToDoItem = ({ todo, index, deleteTodo, toggleTodo }) => {
+  return (
+    <li>
+      <span
+        style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+        onClick={() => toggleTodo(index)}
+      >
+        {todo.title}
+      </span>
+      <button onClick={() => deleteTodo(index)}>删除</button>
+    </li>
+  );
+};
+
+export default ToDoItem;
---
+++ members/STAJJJ/.history/web3front/src/App_20240608144520.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div>
+      "hello"
+    </div>
+
+  );
+}
+
+export default App;
