9f66f9e1a98e624d8be127faf20cfe8101a6f022
gnls666 <185263317@qq.com>
2024-06-09 21:00:41 +0800

    task1:gnls666

---
+++ members/gnls666/task1/to_do_list/src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import TodoList from "./to_do";
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <TodoList />
+    </>
+  );
+}
+
+export default App;
