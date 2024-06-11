82b873daa59bc90411f70c71f8c8dd94c856f514
Billy Ma <billyma128@gmail.com>
2024-06-12 00:05:21 +0800

    task1: add todo application

---
+++ task1/src/App.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './app.css';
+import Header from './header';
+import AddTodo from './add-todo';
+import TodoList from './todo-list';
+
+const App = () => {
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem('todos');
+    if (savedTodos) {
+      return JSON.parse(savedTodos);
+    }
+    return [];
+  });
+
+  const addTodo = (todo: Todo) => {
+    setTodos([...todos, todo]);
+  };
+
+  const toggleTodo = (id: number) => {
+    console.log(id)
+    const newTodos = todos.map((todo: Todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (id: number) => {
+    const newTodos = todos.filter((todo: Todo) => todo.id !== id);
+    setTodos(newTodos);
+  }
+
+  return (
+    <div className="container mx-auto px-4">
+      <Header />
+      <AddTodo addTodo={addTodo} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </div>
+  );
+};
+
+export default App;
