fab8b26bb10c240f179bd0028e0fc209795d4bb1
DESKTOP-QT28NF4\Administrator <su_5482@163.com>
2024-06-12 03:24:36 +0800

    task1

---
+++ members/Yahohohohohoho/task1/src/App.tsx
@@ -0,0 +1,49 @@
+import "./App.css";
+import Header from "./Header";
+import ToDoList from "./ToDoList";
+import AddToDo from "./AddToDo";
+import { useState, useEffect } from "react";
+
+type itemTask = {
+  task: string;
+  isDone: boolean;
+};
+
+function App() {
+  const [toDoList, setToDoList] = useState(localStorage.getItem("toDoList") ? JSON.parse(localStorage.getItem("toDoList") as string) : []);
+  // useEffect(() => {
+  //   const storedList = localStorage.getItem("toDoList");
+  //   if (storedList) {
+  //     try {
+  //       const parsedList = JSON.parse(storedList);
+  //       setToDoList(parsedList);
+  //       console.log("parsedList", parsedList);
+  //     } catch (error) {
+  //       console.error("Error parsing stored tasks:", error);
+  //     }
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem("toDoList", JSON.stringify(toDoList));
+  }, [toDoList]);
+
+  const addItemToList = (item: itemTask) => {
+    const list: itemTask[] = [...toDoList, item];
+    setToDoList(list);
+  };
+
+  const updateToDoList = (list: itemTask[]) => {
+    setToDoList(list);
+  };
+
+  return (
+    <>
+      <Header />
+      <AddToDo addItemToList={addItemToList} />
+      <ToDoList toDoList={toDoList} updateToDoList={updateToDoList} />
+    </>
+  );
+}
+
+export default App;
