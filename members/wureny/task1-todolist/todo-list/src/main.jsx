d3afbffb1a8487e88cfab172f75e61482197afc5
wureny <wurenyu19@gmail.com>
2024-06-12 19:15:05 +0800

    task1: wureny

---
+++ members/wureny/task1-todolist/todo-list/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client.js'
+import App from './App.jsx.js'
+import './index.css'
+import {MyApp} from './MyApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ // <React.StrictMode>
+    <MyApp />
+  //</React.StrictMode>,
+)
---
+++ members/wureny/task1-todolist/todo-list/src/mybutton.jsx
@@ -0,0 +1,17 @@
+function MyButton() {
+    return (
+      <button>
+        I'm a button
+      </button>
+    );
+  }
+  
+  export default function MyApp() {
+    return (
+      <div>
+        <h1>Welcome to my app</h1>
+        <MyButton />
+      </div>
+    );
+  }
+  
