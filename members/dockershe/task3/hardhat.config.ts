74380b6f6098ad68a1fc084de404eff4b0e1003d
docker <dockershe@gmail.com>
2024-06-13 09:41:44 +0800

    task3

--- members/dockershe/task3/hardhat.config.ts
+++ members/dockershe/task3/hardhat.config.ts
@@ -1,8 +1,30 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+
+import "@nomicfoundation/hardhat-ignition-ethers";
+
+
+require('dotenv').config();
+
+
+
+// 在使用 process.env.PRIVATE_KEY 前，检查它是否被定义且不为空，或者使用默认值。这样可以确保传递给 accounts 的值总是字符串类型。
+if (!process.env.PRIVATE_KEY) {
+  throw new Error('PRIVATE_KEY environment variable is not set.');
+}
+
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks:{
+    sepolia:{
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+
+  }
+  
 };
 
 export default config;
---
+++ members/dockershe/task3/scripts/deploy.ts
@@ -0,0 +1,41 @@
+
+import { ethers } from "hardhat";
+
+
+
+async function main() {
+  const [owner] = await ethers.getSigners();
+
+  // 查询账户余额
+  const balance = await owner.provider.getBalance(owner.address);
+  
+  // 将Wei转换为ETH以便于阅读
+  const balanceInEth = ethers.formatEther(balance);
+
+  // 打印账户地址及余额
+  console.log(`Deploying contracts with the account: ${owner.address}, Balance: ${balanceInEth} ETH`);
+
+
+  // 调用函数部署第一个合约
+  const LighthouseNftFactory = await ethers.getContractFactory('LighthouseNft', owner);
+  const lighthouseNft = await LighthouseNftFactory.deploy("YourNFTName", "NFT");
+  // 获取合约地址
+  const contractAddress = await lighthouseNft.getAddress();
+  // 打印合约地址
+  console.log("LighthouseNft contract deployed to:", contractAddress);
+
+  // 调用函数部署第二个合约
+  const NftMarketFactory = await ethers.getContractFactory('NftMarket', owner);
+  const nftMarket = await NftMarketFactory.deploy();
+  const nftMarketContractAddress = await nftMarket.getAddress();
+  console.log("NftMarket contract deployed to:", nftMarketContractAddress);
+
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
