a391aaae652110bbcce752d0d828639e212d1065
Yaqioooong <yaqiongko@gmail.com>
2024-06-13 23:24:44 +0800

    task1:Yaqioooong

--- members/Yaqioooong/task1/README.md
+++ members/Yaqioooong/task1/README.md
@@ -6,3 +6,14 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+```shell
+npm i
+```
+启动项目:
+```shell
+npm run dev
+```
+效果图：
+
+![](./PixPin_2024-06-13_23-20-09.jpg)
--- members/Yaqioooong/task1/src/components/ToDoList.jsx
+++ members/Yaqioooong/task1/src/components/ToDoList.jsx
@@ -1,11 +1,19 @@
-import React, {useState} from "react";
+import React, {useState,useEffect} from "react";
 import ToDoItem from "./ToDoItem.jsx";
 import AddToDo from "./AddToDo.jsx";
 import Header from "./Header.jsx";
 
 
 function ToDoList() {
-    const [todos, setTodos] = useState([]);
+    const [todos, setTodos] = useState(()=>{
+        return JSON.parse(localStorage.getItem("todos")) || [];
+    });
+
+    // 从localStorage加载数据
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
+
     const handleAddTodo = (todoText) => {
         const newTodo = {
             id: Date.now(),
@@ -13,13 +21,23 @@ function ToDoList() {
             completed: false,
         };
         setTodos([...todos, newTodo]);
+        localStorage.setItem('todos', JSON.stringify([...todos, newTodo]));
     };
+
     const handleToggleTodo = (id) => {
-        setTodos(todos.map((todo) => (todo.id === id ? { ...todo, completed: !todo.completed } : todo)));
+        setTodos(
+            todos.map((todo) =>
+                todo.id===id?{...todo,completed: !todo.completed}:todo
+            )
+        );
     };
+
     const handleDeleteTodo = (id) => {
-        setTodos(todos.filter((todo) => todo.id !== id));
+        const updatedTodos = todos.filter((todo) => todo.id !== id);
+        setTodos(updatedTodos);
+        localStorage.setItem('todos', JSON.stringify(updatedTodos));
     };
+
     return (
         <div className="todo-list">
             <Header/>
