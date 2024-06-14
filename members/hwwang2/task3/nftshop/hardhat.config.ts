f355e9357ecf66047bc7bcfd2863e1887e8dc685
hwwang2 <x@x.com>
2024-06-14 18:54:45 +0800

    完成token 和nft的 合约和测试脚本

---
+++ members/hwwang2/task3/nftshop/hardhat.config.ts
@@ -0,0 +1,27 @@
+import { HardhatUserConfig, vars } from "hardhat/config";
+
+import "@nomicfoundation/hardhat-toolbox";
+
+require('dotenv').config();
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    local:{
+      url: "http://127.0.0.1:8545/",
+      accounts:["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"] 
+      //0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 0x70997970C51812dc3A010C7d01b50e0d17dc79C8
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts:[`${process.env.SEPOLIA_ACCOUNT_PRIVATE}`] 
+    },
+  }
+};
+
+export default config;
