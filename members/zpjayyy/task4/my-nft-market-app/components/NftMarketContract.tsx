fba255d113cfa8033ec3c7d4f932663ed53b0de7
zpjayyy <jay.lichzp@gmail.com>
2024-06-15 21:30:34 +0800

    feat: list and purchase

--- members/zpjayyy/task4/my-nft-market-app/components/NftMarketContract.tsx
+++ members/zpjayyy/task4/my-nft-market-app/components/NftMarketContract.tsx
@@ -1,8 +1,11 @@
-import { useWriteContract } from "wagmi";
-import { FormEvent } from "react";
+import { useWatchContractEvent, useWriteContract } from "wagmi";
+import { FormEvent, useState } from "react";
 import { nftMarketContractConfig } from "@/config/nftMarketContractConfig";
-import {Input} from "@nextui-org/input";
-import {Button} from "@nextui-org/button";
+import { Input } from "@nextui-org/input";
+import { Button } from "@nextui-org/button";
+import { nftContractConfig } from "@/config/nftContractConfig";
+import { tokenContractConfig } from "@/config/tokenContractConfig";
+import { ethers } from "ethers";
 
 export default function NftMarketContract() {
   return (
@@ -15,21 +18,45 @@ export default function NftMarketContract() {
 
 function List() {
   const { data: hash, writeContract } = useWriteContract();
+  const [tokenId, setTokenId] = useState<bigint>();
+  const [price, setPrice] = useState<bigint>();
+
+  useWatchContractEvent({
+    ...nftContractConfig,
+    eventName: "Approval",
+    onLogs(logs) {
+      console.log(logs);
+      // approved
+      if (tokenId && price) {
+        list(tokenId, price).catch((error) => console.log(error));
+      }
+    },
+  });
+
+  async function approve(tokenId: bigint) {
+    writeContract({
+      ...nftContractConfig,
+      functionName: "approve",
+      args: [nftMarketContractConfig.address, BigInt(tokenId)],
+    });
+  }
+
+  async function list(tokenId: bigint, price: bigint) {
+    writeContract({
+      ...nftMarketContractConfig,
+      functionName: "list",
+      args: [nftContractConfig.address, BigInt(tokenId), BigInt(price)],
+    });
+  }
 
   async function submit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const formDate = new FormData(e.target as HTMLFormElement);
-    const tokenId = formDate.get("tokenId") as string;
-    const price = formDate.get("price") as string;
-    writeContract({
-      ...nftMarketContractConfig,
-      functionName: "list",
-      args: [
-        "0x452BFD557b3a4Fc39ACB4bbdC0314720012B3395",
-        BigInt(tokenId),
-        BigInt(price),
-      ],
-    });
+    const tokenId = BigInt(formDate.get("tokenId") as string);
+    const price = BigInt(formDate.get("price") as string);
+    setTokenId(tokenId);
+    setPrice(price);
+    await approve(tokenId);
   }
 
   return (
@@ -47,15 +74,42 @@ function List() {
 function Purchase() {
   const { data: hash, error, writeContract } = useWriteContract();
 
-  async function submit(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    const formDate = new FormData(e.target as HTMLFormElement);
-    const tokenId = formDate.get("tokenId") as string;
+  const [tokenId, setTokenId] = useState<bigint>();
+
+  useWatchContractEvent({
+    ...tokenContractConfig,
+    eventName: "Approval",
+    onLogs(logs) {
+      console.log(logs);
+      // approved
+      if (tokenId) {
+        purchase(tokenId).catch((error) => console.log(error));
+      }
+    },
+  });
+
+  async function approve() {
+    writeContract({
+      ...tokenContractConfig,
+      functionName: "approve",
+      args: [nftMarketContractConfig.address, ethers.parseEther("1")],
+    });
+  }
+
+  async function purchase(tokenId: bigint) {
     writeContract({
       ...nftMarketContractConfig,
       functionName: "purchase",
-      args: ["0x452BFD557b3a4Fc39ACB4bbdC0314720012B3395", BigInt(tokenId)],
+      args: [nftContractConfig.address, BigInt(tokenId)],
     });
+  }
+
+  async function submit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formDate = new FormData(e.target as HTMLFormElement);
+    const tokenId = BigInt(formDate.get("tokenId") as string);
+    setTokenId(tokenId);
+    await approve();
   }
 
   return (
