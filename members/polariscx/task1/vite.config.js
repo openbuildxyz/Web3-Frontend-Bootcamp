e6de056720f2ec842b77ac6609e733b351077abd
polariscx <105368178+polariscx@users.noreply.github.com>
2024-06-15 19:22:10 +0800

    Normalize line endings

--- members/polariscx/task1/vite.config.js
+++ members/polariscx/task1/vite.config.js
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preprocessorOptions:{
-    less:{
-      javascriptEnabled:true,
-      modifyVars:{
-      }
-}
-}
 })
