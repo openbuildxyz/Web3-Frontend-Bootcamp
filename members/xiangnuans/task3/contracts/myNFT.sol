120470387c56c63d5321516472b750d7a6b58811
Guo Hong <1352118502@qq.com>
2024-06-15 21:45:07 +0800

    feat: 初始化密码文件

---
+++ members/xiangnuans/task3/contracts/myNFT.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721, Ownable {
+    uint256 private _tokenIdCounter;
+
+    constructor() ERC721("MyNFT", "MNFT") Ownable(msg.sender) {}
+
+    function mint(address to) public onlyOwner {
+        _safeMint(to, _tokenIdCounter);
+        _tokenIdCounter++;
+    }
+}
