bd2254f6eaac8461f2349be984cb5f93e910f4a7
hwwang2 <x@x.com>
2024-06-15 15:30:16 +0800

    修改配置

--- members/hwwang2/task3/nftshop/hardhat.config.ts
+++ members/hwwang2/task3/nftshop/hardhat.config.ts
@@ -14,7 +14,7 @@ const config: HardhatUserConfig = {
     },
     local:{
       url: "http://127.0.0.1:8545/",
-      accounts:["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"] 
+      accounts:["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d", "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a"] 
       //0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 0x70997970C51812dc3A010C7d01b50e0d17dc79C8
     },
     sepolia: {
