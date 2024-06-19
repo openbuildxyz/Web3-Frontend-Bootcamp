ebab393e1cf6c3474c572486fb2270cc64a620a1
Jeffrey <25268655@qq.com>
2024-06-20 07:03:11 +0800

    添加Header组件

--- members/jeff09-28/task1/src/App.jsx
+++ members/jeff09-28/task1/src/App.jsx
@@ -1,12 +1,12 @@
-import {useState} from 'react'
 import './App.css'
+import Header from "./ components/Header.jsx";
 
 function App() {
-    const [count, setCount] = useState(0)
 
     return (
         <>
             <div className="card">
+                <Header/>
                 <button>
                     添加
                 </button>
