f54d37b84cd1140ba7d8d691866d04547a76cecc
no5no6 <work_yuanyang@163.com>
2024-06-19 20:36:40 +0800

    feat: task1

---
+++ members/no5no6/task1/src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import Home from '@/pages/home'
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ members/no5no6/task1/src/pages/home/index.jsx
@@ -0,0 +1,16 @@
+
+// interface HomeProps {
+
+// }
+
+
+function Home () {
+
+  return (
+    <div>
+      <div>我是首页</div>
+    </div>
+  )
+}
+
+export default Home
