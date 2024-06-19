de69bcc4a46f45f33af12e02ea860f4428e17417
sunhaoran <1208818417@qq.com>
2024-06-19 15:53:34 +0800

    OBToken

---
+++ members/LittleMoreInteresting/task3/contracts/OBToken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+pragma solidity ^0.8.24;
+
+contract OBToken is ERC20 {
+    constructor() ERC20("Open Build Token","OBT") {
+           
+    }
+}
