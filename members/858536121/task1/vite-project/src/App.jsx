9cc5e3f0d592173fa05fc1d08a0717a4561291b0
lincunshuo1 <lincunshuo1@jd.com>
2024-06-20 16:55:06 +0800

    AddToDo

--- members/858536121/task1/vite-project/src/App.jsx
+++ members/858536121/task1/vite-project/src/App.jsx
@@ -1,17 +1,25 @@
-import { useState } from 'react'
-// import './App.css'
-import Header from './todoComponents/Header'
-function App() {
-  const [count, setCount] = useState(0)
+import { useState } from "react";
+import "./App.css";
+import Header from "./todoComponents/Header";
+import AddToDo from "./todoComponents/AddToDo";
 
+function App() {
+  const [todoList, setTodoList] = useState([]);
+  const onAddClick = (val) => {
+    if (val) {
+      todoList.push({ label: val, isOK: false });
+      setTodoList([...todoList]);
+      console.log(todoList);
+    }
+  };
   return (
     <>
-      <Header/>
-      <div>
-        Tom
+      <div className={"header"}>
+        <Header />
       </div>
+      <AddToDo onAddClick={onAddClick} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
