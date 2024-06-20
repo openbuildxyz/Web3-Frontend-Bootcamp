18ffb7bd1f6127f4068e216e4477f8511d24ad3c
secret <18364307619@163.com>
2024-06-20 15:59:50 +0800

    完成MyToken测试

--- members/Secret1007/task3/test/MyToken.ts
+++ members/Secret1007/task3/test/MyToken.ts
@@ -10,42 +10,44 @@ describe("MyToken", function () {
         return { erc20, owner, otherAccount };
     }
 
-    // describe("Deployment", function () {
-    //     it("Should set the right token decimal", async function () {
-    //         const { erc20 } = await loadFixture(deployErc20Token);
-    //         expect(await erc20.decimals()).to.equal(18n);
-    //     });
-    // });
+    describe("Deployment", function () {
+        it("Should set the right token decimal", async function () {
+            const { erc20 } = await loadFixture(deployErc20Token);
+            expect(await erc20.decimals()).to.equal(18n);
+        });
+    });
 
-    describe("Transfers", function () {
-        // describe("Validations", function () {
-        //     it("Should revert with the right error if not the owner", async function () {
-        //         const { erc20, owner, otherAccount } = await loadFixture(deployErc20Token);
-        //         await expect(erc20.connect(otherAccount).mintTo(otherAccount.address, 100)).to.be.revertedWith("Ownable: caller is not the owner");
-        //     });
-        //     it("Should not revert if mintTo is called by the owner", async function () {
-        //         const { erc20, owner, otherAccount } = await loadFixture(deployErc20Token);
-        //         await expect(erc20.connect(owner).mintTo(otherAccount.address, 100)).not.to.be.reverted;
-        //     });
-        //     it("Should not revert if mint is called with ETH", async function () {
-        //         const { erc20, owner, otherAccount } = await loadFixture(deployErc20Token);
-        //         await expect(erc20.connect(otherAccount).mint({ value: ethers.parseEther("0.1") })).not.to.be.reverted;
-        //     });
-        // });
-        // describe("Events", function () {
-        //     it("Should emit transfer event", async function () {
-        //         const { erc20, owner, otherAccount } = await loadFixture(deployErc20Token);
-        //         await expect(erc20.connect(owner).mintTo(otherAccount.address, 1000)).to.emit(erc20, "Transfer").withArgs(ethers.constants.AddressZero, otherAccount.address, 1000);
-        //     });
-        // });
-        // it("Should cost the right ether.", async function () {
-        //     const { erc20, owner, otherAccount } = await loadFixture(deployErc20Token);
-        //     await expect(() => erc20.connect(otherAccount).mint({ value: ethers.parseEther("0.1") })).to.changeEtherBalance(otherAccount, ethers.parseEther("0.1"));
-        // });
-        // it("Should mint the right amount of tokens", async function () {
-        //     const { erc20, owner, otherAccount } = await loadFixture(deployErc20Token);
-        //     await expect(() => erc20.connect(otherAccount).mint({ value: ethers.parseEther("0.1") })).to.changeTokenBalance(erc20, otherAccount, ethers.parseEther("0.1").mul(10));
-        // });
+    describe("mint", function () {
+        describe("Validations", function () {
+            it("Should revert with the right error if not the owner", async function () {
+                const { erc20, owner, otherAccount } = await loadFixture(deployErc20Token);
+                await expect(erc20.connect(otherAccount).mintTo(otherAccount, 100)).to.be.revertedWithCustomError(erc20, "OwnableUnauthorizedAccount");
+            });
+            it("Should not revert if mintTo is called by the owner", async function () {
+                const { erc20, owner, otherAccount } = await loadFixture(deployErc20Token);
+                await expect(erc20.connect(owner).mintTo(otherAccount.address, 100)).not.to.be.reverted;
+            });
+            it("Should not revert if mint is called with ETH", async function () {
+                const { erc20, owner, otherAccount } = await loadFixture(deployErc20Token);
+                await expect(erc20.connect(otherAccount).mint({ value: ethers.parseEther("0.1") })).not.to.be.reverted;
+            });
+        });
+
+        describe("Events", function () {
+            it("Should emit transfer event", async function () {
+                const { erc20, owner, otherAccount } = await loadFixture(deployErc20Token);
+                await expect(erc20.connect(owner).mintTo(otherAccount, 1000)).to.emit(erc20, "Transfer").withArgs(ethers.ZeroAddress, otherAccount, 1000); // We accept any value as `when` arg
+            });
+        });
+
+        it("Should cost the right ether.", async function () {
+            const { erc20, owner, otherAccount } = await loadFixture(deployErc20Token);
+            await expect(() => erc20.connect(otherAccount).mint({ value: ethers.parseEther("0.1") })).to.changeEtherBalance(otherAccount, -ethers.parseEther("0.1"));
+        });
+        it("Should mint right value.", async function () {
+            const { erc20, owner, otherAccount } = await loadFixture(deployErc20Token);
+            await expect(erc20.connect(otherAccount).mint({ value: ethers.parseEther("0.1") })).changeTokenBalance(erc20, otherAccount, ethers.parseEther("0.1") * BigInt(10));
+        });
     });
 
     describe("Transfers", function () {
