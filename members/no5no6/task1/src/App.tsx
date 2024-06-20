e551a29779bf3540b51f812fc4b0e7c6da3ef725
no5no6 <work_yuanyang@163.com>
2024-06-20 16:00:50 +0800

    task1:no5no6

--- members/no5no6/task1/src/App.tsx
+++ members/no5no6/task1/src/App.tsx
@@ -1,14 +1,64 @@
-// import { useState } from 'react'
-import Home from '@/pages/home'
+import { useState, useEffect } from 'react'
+
+import { ITodoItem } from '@/types'
+
+import Header from '@/components/header'
+import TodoList from '@/components/todoList'
+import TodoItem from './components/todoItem'
+import AddToDo from './components/addToDo'
 
 
 function App() {
-  // const [count, setCount] = useState(0)
+  // 缓存数据 key
+  const KEY = 'TODO_LIST'
+
+  // 从缓存中拿取记录
+  const localListData = JSON.parse(localStorage.getItem(KEY) || '[]')
+
+  // 待办事件列表
+  const [todoList, setTodoList] = useState<ITodoItem[]>(localListData)
+
+  // 监控 todoList 的变化，当 todoList 变化时，将 todoList 存入缓存
+  useEffect(() => {
+    localStorage.setItem(KEY, JSON.stringify(todoList))
+  }, [todoList])
+
+  // 添加待办事项
+  const onAddTask = (item: ITodoItem) => {
+    setTodoList([...todoList, item])
+  }
+
+  // 更新待办事项
+  const onUpdateTask = (item: ITodoItem) => {
+
+    // 更新的数据覆盖原有数据
+    const newTodoList = todoList.map(todo => todo.id === item.id ? {...item} : todo)
+    // 更新 todoList
+    setTodoList(newTodoList)
+  }
+
+  // 
+  const onDeleteTask = (id: number) => {
+    const newTodoList = todoList.filter(todo => todo.id !== id)
+    setTodoList(newTodoList)
+  }
 
   return (
-    <div>
-      <Home />
-    </div>
+    <>
+      <div className="w-full h-screen flex flex-justify-center flex-items-center">
+        <div className="w-lg">
+          <Header />
+          <AddToDo onAddTask={ onAddTask } />
+          <TodoList >
+            {
+              todoList.map(item => (
+                <TodoItem key={item.id} item={item} onUpdateTask={ onUpdateTask } onDeleteTask={ onDeleteTask } />
+              ))
+            }
+          </TodoList>
+        </div>
+      </div>
+    </>
   )
 }
 
--- members/no5no6/task1/vite.config.ts
+++ members/no5no6/task1/vite.config.ts
@@ -1,13 +1,14 @@
+import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), UnoCSS()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+      '@': resolve(__dirname, 'src')
+    },
+  },
 })
