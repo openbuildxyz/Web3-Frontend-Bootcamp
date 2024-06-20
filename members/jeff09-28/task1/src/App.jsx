8517f244defa7d0548709d30f1d0898d6c5ab1e9
Jeffrey <25268655@qq.com>
2024-06-20 10:04:31 +0800

    添加todo状态管理的、todo的添加与删除函数

--- members/jeff09-28/task1/src/App.jsx
+++ members/jeff09-28/task1/src/App.jsx
@@ -1,16 +1,41 @@
 import './App.css'
 import Header from "./components/Header.jsx";
-import {ToDoList} from "./components/ToDoList.jsx";
+import {ToDoList} from "./components/ToDoList.jsx"
+import {AddToDo} from "./components/AddToDo.jsx"
+import {useEffect, useState} from "react"
 
 function App() {
+    // 初始化状态管理
+    const [todos, setTodos] = useState(() => {
+        const savedTodos = localStorage.getItem('todos')
+        return savedTodos ? JSON.parse(savedTodos) : []
+    })
+
+    // 监听 todos 变化，并将其保存到本地存储
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    }, [todos])
+
+    // 添加与更新 todos 的函数
+    const addTodo = (test) => {
+        const newTodos = {
+            id: Date.now(),
+            text: test,
+            completed: false
+        }
+        setTodos([...todos, newTodos])
+    }
+    // 删除指定id的todo
+    const deleteTodo = (id) => {
+        setTodos(todos.filter(todo => todo.id !== id))
+    }
 
     return (
-        <>
-            <div className="card">
-                <Header/>
-                <ToDoList></ToDoList>
-            </div>
-        </>
+        <div className="card">
+            <Header/>
+            <AddToDo addTodo={addTodo} todos={todos}></AddToDo>
+            <ToDoList deleteTodo={deleteTodo} todos={todos}></ToDoList>
+        </div>
     )
 }
 
