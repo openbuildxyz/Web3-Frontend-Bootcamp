b58155e75263c60d37c7e1a4e111497f80f6538d
charming-06 <1161660087@qq.com>
2024-06-22 17:05:18 +0800

    增加本地存储

--- members/charming-06/task1/vite-project/src/App.tsx
+++ members/charming-06/task1/vite-project/src/App.tsx
@@ -3,26 +3,34 @@ import './App.css'
 import ToDoList from './components/ToDoList'
 import Header from './components/Header'
 import AddToDo from './components/AddToDo'
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+// 定义接口
+interface todoItemProps {
+    index: number;
+    msg: string;
+    status: boolean;
+}
 function App() {
     // 从缓存中取出待办事项
     const data = localStorage.getItem("list");
     const result = data ? JSON.parse(data) : [];
     const [list, setList] = useState(result);
     const [title] = useState('我的待办事项')
+    
+    useEffect(() => {
+        // 在这里使用更新后的值
+        console.log('list updated:', list);
+        localStorage.setItem("list", JSON.stringify(list));
+    }, [list]); // 依赖于 list
 
+    // 增加一个待办事项
     const addNewToDo = (msg: string, status: boolean) => {
-        console.log('msg', msg);
-
         const newToDo = { index: list.length + 1, msg, status };
         setList([...list, newToDo]);
     };
+    // 删除一个待办事项
     const deleteToDo = (index: number) => {
-        console.log('App index', index);
-        const result = list.filter(item => item.index !== index);
-        setList(result);
-        localStorage.setItem("list", JSON.stringify(result));
+        setList(list.filter((item: todoItemProps) => item.index !== index));
     };
 
     return (
