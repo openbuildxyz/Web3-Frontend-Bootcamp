336b660ac2b828ba45324217b05ea0cb98053979
mecrev-secret <18364307619@163.com>
2024-06-23 16:40:41 +0800

    fix: 修改合约引入路径

--- members/Secret1007/task3/contracts/NFTMarket.sol
+++ members/Secret1007/task3/contracts/NFTMarket.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 contract NFTMarketplace is ReentrancyGuard {
     IERC20 public erc20Token;
