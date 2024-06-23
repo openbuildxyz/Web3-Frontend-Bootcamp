737a0a3c21be82b618a450dbfedd0007fdaad998
mecrev-secret <18364307619@163.com>
2024-06-23 18:08:31 +0800

    feat: 更新合约测试文件

--- members/Secret1007/task3/contracts/NFTMarket.sol
+++ members/Secret1007/task3/contracts/NFTMarket.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-contract NFTMarketplace is ReentrancyGuard {
+contract NFTMarket is ReentrancyGuard {
     IERC20 public erc20Token;
     address public owner;
 
