4eec8211eb9b920647a68e4cf13f3b02d8bca027
suxin123 <2418213125@qq.com>
2024-06-24 17:47:17 +0800

    ERC20

--- members/Yahohohohohoho/task3/OpenBuildToken.sol
+++ members/Yahohohohohoho/task3/OpenBuildToken.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract OpenBuildToken is ERC20 {
-    constructor() ERC20("OpenBuildToken", "0BT") {
+contract OpenBuildToken is ERC20, Ownable {
+    constructor(address initialOwner) ERC20("OpenBuildToken", "0BT") Ownable(initialOwner) {
         _mint(msg.sender, 10000000000 * 10 * 6);
     }
 
-    function decimals() public view virtual override returns (uint8) {
-        return 6;
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
     }
 }
