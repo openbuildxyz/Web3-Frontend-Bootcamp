dde881cfbdcd3845ba9dee4c57609de122de451a
huzhengen <huzhengen@163.com>
2024-06-24 08:37:24 +0800

    update task4

---
+++ members/huzhengen/task4/my-app/abis/allenTokenABI.ts
@@ -0,0 +1,26 @@
+export const allenTokenABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "value",
+        "type": "uint256"
+      }
+    ],
+    "name": "transfer",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+] as const
