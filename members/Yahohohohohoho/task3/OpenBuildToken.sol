06b22278471a6f1a455e9ccf4594be228b45c11c
suxin123 <2418213125@qq.com>
2024-06-24 17:17:40 +0800

    task3初始化

---
+++ members/Yahohohohohoho/task3/OpenBuildToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract OpenBuildToken is ERC20 {
+    constructor() ERC20("OpenBuildToken", "0BT") {
+        _mint(msg.sender, 10000000000 * 10 * 6);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
+}
