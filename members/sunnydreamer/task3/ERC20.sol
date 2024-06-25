c0fabcc1a57ffb7cb1a49a1b2c08fe6961187196
sunnydreamer <sunnylee9516@gmail.com>
2024-06-25 00:15:24 -0400

    update ERC20

---
+++ members/sunnydreamer/task3/ERC20.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts@5.0.2/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
+
+contract SunToken is ERC20, Ownable {
+    constructor(address initialOwner)
+        ERC20("SunToken", "SUN")
+        Ownable(initialOwner)
+    {
+        _mint(msg.sender, 10000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
