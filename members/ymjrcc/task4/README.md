bffed629282ac31ff1de7118fbc352b920b220de
heyiming <ymjrcc@qq.com>
2024-06-27 21:15:22 +0800

    task4: ymjrcc

---
+++ members/ymjrcc/task4/README.md
@@ -0,0 +1,8 @@
+- 完整代码：
+  - 合约代码位于 `/hardhat/contract`
+  - 前端代码位于 `/nextjs/app/list` 和 `/nextjs/app/purchase`
+
+界面截图：
+![连接钱包](连接钱包.jpg)
+![上架NFT](上架NFT.jpg)
+![显示上架的NFT并购买](显示上架的NFT并购买.jpg)
---
+++ members/ymjrcc/task4/nextjs/app/layout.tsx
@@ -0,0 +1,24 @@
+import "@rainbow-me/rainbowkit/styles.css";
+import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithProviders";
+import { ThemeProvider } from "~~/components/ThemeProvider";
+import "~~/styles/globals.css";
+import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
+
+export const metadata = getMetadata({
+  title: "Scaffold-ETH 2 App",
+  description: "Built with 🏗 Scaffold-ETH 2",
+});
+
+const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html suppressHydrationWarning>
+      <body>
+        <ThemeProvider enableSystem>
+          <ScaffoldEthAppWithProviders>{children}</ScaffoldEthAppWithProviders>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+};
+
+export default ScaffoldEthApp;
