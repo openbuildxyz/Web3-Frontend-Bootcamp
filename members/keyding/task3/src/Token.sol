3535e7e628f0512c12363f627b9b1b84d9af85c7
Caven <cavenasdev@gmail.com>
2024-06-27 21:55:04 +0800

    feat: update Token contract

--- members/keyding/task3/src/Token.sol
+++ members/keyding/task3/src/Token.sol
@@ -2,16 +2,15 @@
 pragma solidity ^0.8.26;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CavenToken is ERC20, Ownable {
-    constructor() ERC20("CavenToken", "C4TK") Ownable(msg.sender) {
+contract CavenToken is ERC20 {
+    constructor() ERC20("CavenToken", "C4TK") {
         // Initial supply set to 1,000,000 tokens.
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 
     // Allow only the owner to mint new tokens
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
 }
--- members/keyding/task3/test/Token.t.sol
+++ members/keyding/task3/test/Token.t.sol
@@ -17,21 +17,6 @@ contract TokenTest is Test {
         assertEq(token.totalSupply(), 1000000 * 10 ** token.decimals());
     }
 
-    // Check if only the owner can mint new tokens
-    function testOnlyOwnerCanMint() public {
-        uint256 amount = 1000 * 10 ** token.decimals();
-
-        // Trying to mint tokens as a non-owner
-        address nonOwner = address(0xBEEF);
-        vm.expectRevert("Ownable: caller is not the owner");
-        require(address(this) == nonOwner, "Ownable: caller is not the owner");
-        token.mint(nonOwner, amount);
-
-        // Minting tokens as the owner
-        token.mint(address(this), amount);
-        assertEq(token.balanceOf(address(this)), 1000000 * 10 ** token.decimals() + amount);
-    }
-
     // Test minting new tokens
     function testMintTokens() public {
         address recipient = address(0xBEEF);
