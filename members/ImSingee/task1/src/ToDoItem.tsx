e3b95879a743161eab637218f24a1a6424858d93
Bryan <git@singee.me>
2024-06-28 22:33:32 +0800

    support delete

--- members/ImSingee/task1/src/ToDoItem.tsx
+++ members/ImSingee/task1/src/ToDoItem.tsx
@@ -1,9 +1,24 @@
 import {Item} from "./todo.ts";
+import {useSetTodoItems} from "./TodoListProvider.tsx";
 
 export type Props = {
     item: Item
 }
 
 export function ToDoItem({item}: Props) {
-    return <div>{item.title}</div>
+    const setItems = useSetTodoItems()
+
+    function handleDelete() {
+        setItems((items) => items.filter((i) => i !== item))
+    }
+
+    return (
+        <div className="flex">
+            <label>
+                <input type="checkbox" checked={item.completed}/>
+                {item.title}
+            </label>
+            <div className="delete" onClick={handleDelete}>Delete</div>
+        </div>
+    )
 }
