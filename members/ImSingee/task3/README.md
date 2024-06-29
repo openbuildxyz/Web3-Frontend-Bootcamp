9999f181b4b3217facc2ec289cb7ce3741985c4a
Bryan <git@singee.me>
2024-06-29 16:47:49 +0800

    add nft and token

---
+++ members/ImSingee/task3/README.md
@@ -0,0 +1,13 @@
+测试账户：
+  - 账户一：0xeb4fb7818f0A5A17AD8c026B35fA3a0Fc189F639
+  - 账户二：0x2390f96d3a47B508De4f8899cc6a116D630E1A0C
+
+代币合约：
+  - 地址：0x05c4e95b7a59c89eE0a0BF915631E2f5923B283b https://sepolia.etherscan.io/address/0x05c4e95b7a59c89ee0a0bf915631e2f5923b283b
+  - 名称：BryanCoin BRC
+
+NFT 合约：
+  - 地址：0xB880B6594D8d398A430fc6bD42f4ea32b31cE932 https://sepolia.etherscan.io/address/0xb880b6594d8d398a430fc6bd42f4ea32b31ce932
+  - 名称：BryanNFT BNFT
+
+账户 1 mint NFT （获得了 0 号 NFT） 的 transaction：https://sepolia.etherscan.io/tx/0x1b6c36b3a0baf9c6a3007c4e057f20043685d786d1d14fa5cc5c468bb4eb1780
---
+++ members/ImSingee/task3/contracts/nft.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract BryanNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("BryanNFT", "BNFT") {}
+
+    function awardItem(address player, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        uint256 newItemId = _tokenIds.current();
+        _mint(player, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        _tokenIds.increment();
+        return newItemId;
+    }
+}
