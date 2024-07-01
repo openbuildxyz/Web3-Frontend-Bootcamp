43ce6be04468b89156f1c5221e432a15542b8f2c
lispking <lispking@qq.com>
2024-07-02 00:06:53 +0800

    update wrong review

--- members/lispking/task3/scripts/config.ts
+++
@@ -1,3 +0,0 @@
-export const tokenAddress = '0xE69DCaE5A6cA1DCE3beBfd5264a83436951c6F43';
-export const nftAddress = '0xdD65F5D3a5AEE68769267a5663CCcD213b45ABaA';
-export const nftMarketAddress = '0xEE9e3a49eCA927933Ef08c742ce2E0a89bEDd419';
