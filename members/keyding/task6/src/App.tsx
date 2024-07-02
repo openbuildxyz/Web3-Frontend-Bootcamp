220ef04db39518ab5f0843fff2d6d042e58b3c3e
Caven <cavenasdev@gmail.com>
2024-07-02 17:43:11 +0800

    feat: init

---
+++ members/keyding/task6/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center">
+        <Button>Hello World</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ members/keyding/task6/vite.config.ts
@@ -0,0 +1,13 @@
+import { resolve } from "node:path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+})
