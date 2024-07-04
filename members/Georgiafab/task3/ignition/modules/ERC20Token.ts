e742e930eb387f55c94878a4ea01229ccc3fe774
Georgiafab <2369596999@qq.com>
2024-07-04 11:21:14 +0800

    fix: 删除多余文件

--- members/Georgiafab/task3/ignition/modules/ERC20Token.ts
+++
@@ -1,5 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-module.exports = buildModule("ERC20TokenModule", (m) => {
-  const erc20token = m.contract("ERC20Token", ["myErc20", "¥", 1000]);
-  return { erc20token };
-});
--- members/Georgiafab/task3/ignition/modules/ERC721Token.ts
+++
@@ -1,5 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-module.exports = buildModule("ERC721TokenModule", (m) => {
-  const ERC721Token = m.contract("ERC721Token", ["myErc721", "er"]);
-  return { ERC721Token };
-});
