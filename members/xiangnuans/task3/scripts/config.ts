a8a87d125b7b0a6f570b8505ecb278fb3c927300
Guo Hong <1352118502@qq.com>
2024-07-04 14:50:38 +0800

    refactor: pr change request delete files

--- members/xiangnuans/task3/scripts/config.ts
+++
@@ -1,4 +0,0 @@
-// etherscan的合约地址
-export const tokenAddress = "0x1DaFFf1879700220Aa559276E72c4468DAB8768D";
-export const nftAddress = '0xA91B95f362e5B56c5502750e94874F27B986B0B2';
-export const marketAddress = '0x47F9eB89EB8f43f1747dA3Bb4cba5C4e450b8B6E';
