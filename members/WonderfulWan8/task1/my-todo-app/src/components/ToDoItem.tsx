5335e28e840f3e581b57e7ee453c0b7115f2a3e8
fudengwei <fudengweiw@gmail.com>
2024-07-07 11:33:47 +0800

    feat:complete_checkbox

--- members/WonderfulWan8/task1/my-todo-app/src/components/ToDoItem.tsx
+++ members/WonderfulWan8/task1/my-todo-app/src/components/ToDoItem.tsx
@@ -2,27 +2,34 @@
 import React from 'react';
 
 interface ToDoItemProps {
-  todo: {
-    id: number;
-    text: string;
-    completed: boolean;
-  };
-  toggleComplete: (id: number) => void;
-  deleteTodo: (id: number) => void;
+    todo: {
+        id: number;
+        text: string;
+        completed: boolean;
+    };
+    toggleComplete: (id: number) => void;
+    deleteTodo: (id: number) => void;
 }
 
 const ToDoItem: React.FC<ToDoItemProps> = ({ todo, toggleComplete, deleteTodo }) => {
-  return (
-    <li>
-      <span
-        style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
-        onClick={() => toggleComplete(todo.id)}
-      >
-        {todo.text}
-      </span>
-      <button onClick={() => deleteTodo(todo.id)}>完成</button>
-    </li>
-  );
+    return (
+        <li style={{ display: 'flex', alignItems: 'center' }}>
+            <input
+                type='checkbox'
+                checked={todo.completed}
+                onChange={() => toggleComplete(todo.id)}
+                style={{ marginRight: '10px' }}
+            />
+            <span
+                style={{
+                    textDecoration: todo.completed ? 'line-through' : 'none',
+                    marginRight: '10px',
+                }}>
+                {todo.text}
+            </span>
+            <button onClick={() => deleteTodo(todo.id)}>删除</button>
+        </li>
+    );
 };
 
 export default ToDoItem;
