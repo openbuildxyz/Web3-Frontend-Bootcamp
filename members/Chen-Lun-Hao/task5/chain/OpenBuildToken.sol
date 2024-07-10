46bc319bb9358c0662b1a3254bbe88f8ae0731a0
萧 <2085127827@qq.com>
2024-07-10 16:07:49 +0800

    task5: Chen-Lun-Hao

---
+++ members/Chen-Lun-Hao/task5/chain/OpenBuildToken.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract OpenBuildToken is ERC20{
+
+    constructor() ERC20("OpenBuildToken","OBT"){
+        _mint(msg.sender, 100_000_000 * 10 ** 6);
+    }
+
+    //mint OBT
+    function _mint(uint amount) external {
+        _mint(msg.sender, amount);
+    }
+
+
+    //burn OBT
+    function _burn(uint amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    //小数点
+    function decimals() public view virtual override  returns (uint8) {
+        return 6;
+    }
+}
