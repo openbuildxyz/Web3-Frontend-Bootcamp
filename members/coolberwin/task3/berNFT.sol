f985cfab9fa8172a0be6ffa2f7e4e6ea1c4ac4e6
Cat2Boy <coolberwin@hotmail.com>
2024-07-14 20:44:38 +0800

    完成task4

--- members/coolberwin/task3/berNFT.sol
+++ members/coolberwin/task3/berNFT.sol
@@ -1,21 +1,60 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BerNFT is ERC721 {
-    uint256 private MAX_SUPPLY = 1001;
-    uint256 private _nextTokenId = 0;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.9/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.9/contracts/access/Ownable.sol";
 
-    constructor() ERC721("JayApe", "JayApe") {}
+contract BerNFT is ERC721URIStorage ,Ownable{
+    uint256 private _currentTokenId = 0;
+    uint256 public totalSupply;
+    string private baseURI;
 
-    function mint(address to) external {
-        require(_nextTokenId < MAX_SUPPLY, "over max supply");
-        uint256 tokenId = _nextTokenId++;
-        _mint(to, tokenId);
+
+    // constructor(uint256 _maxSupply, string memory _baseURI) ERC721("BerNFT", "BNFT") {
+    //     totalSupply = _maxSupply;
+    //     baseURI = _baseURI;
+    // }
+    
+        // 传递 name 和 symbol 参数到 ERC721 构造函数
+    constructor(uint256 _maxSupply, string memory _baseURI)
+        ERC721("BerNFT", "BNFT") {  // 这里指定了 "BerNFT" 作为 name 和 "BNFT" 作为 symbol
+        totalSupply = _maxSupply;
+        baseURI = _baseURI;
     }
-
-    function _baseURI() internal pure override returns (string memory) {
-        return "ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/";
+    function mint() public returns(uint256) {
+        require(_currentTokenId < totalSupply, "Max supply reached");
+        // require(paymentToken.transferFrom(address(msg.sender),address(this),tokenPrice) , "Insufficient balance");
+        uint256 newTokenId = _currentTokenId++;
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, getTokenURI(newTokenId));
+        
+        return newTokenId;
+    }
+    function getTokenURI(uint256 tokenId) public view returns(string memory) {
+        return string(abi.encodePacked(baseURI, uint2str(9401 + tokenId)));
+    }
+        function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len;
+        while (_i != 0) {
+            k = k-1;
+            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            _i /= 10;
+        }
+        return string(bstr);
     }
 }
