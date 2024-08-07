d3122513b4fecd88ad9d6db06032a4cf18b73cdd
Eric-Cart <ericduuuuu@gmail.com>
2024-08-07 11:50:25 +0800

    task1: update file location

---
+++ members/Eric-Cart/task3/OpenBuildToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract OpenBuildToken is ERC20 {
+    constructor() ERC20("OpenBuildToken", "OBT") {
+        _mint(msg.sender, 10000000000 * 10 ** 6);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
+}
---
+++ members/Eric-Cart/task3/Web3FrontendToken.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { Counters } from "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Web3FrontendToken is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("Web3FrontendToken", "WFT") {
+        _tokenIdCounter.increment();
+    }
+
+    function mint(address to) public {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+}
