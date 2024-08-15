f7603ec3b6b61accf3ec60da83bec818b75fe14c
xuedongZ <zhang.xuedong@infoloop.cn>
2024-08-15 21:09:40 +0800

    feat: todo app

--- members/danileTX/task1/src/App.tsx
+++ members/danileTX/task1/src/App.tsx
@@ -1,18 +1,13 @@
 import { useState, useEffect } from 'react';
-import InputTask from "./InputTask";
-import ListTasks from "./ListTasks";
+import Header from './Header';
+import AddToDo from "./AddToDo";
+import ToDoList from "./ToDoList";
+import {taskItemType} from "./types"
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 import StorageService from "./service.js"
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-
-
-type taskItemType = {
-  id: number;
-  text: string;
-  strike: boolean;
-}
 
 function App() {
   const [tasks, setTasks] = useState({ tasks: [] as taskItemType[] })
@@ -25,43 +20,39 @@ function App() {
 
   const addTasks = (task: string) => {
     const tempTasks: taskItemType[] = tasks["tasks"];
-    const taskItem: taskItemType = { id: tempTasks.length + 1, text: task, strike: false };
+    const taskItem: taskItemType = { id: Date.now().toString(32), text: task, strike: false };
     tempTasks.push(taskItem)
     setTasks({ tasks: tempTasks });
     StorageService.setItem('tasks',tempTasks)
   }
 
-  const removeTasks = (taskid: number) => {
+  const removeTasks = (taskid: string) => {
     const tempTasks = tasks["tasks"];
-    const result: taskItemType[] = [];
-    for (let i = 0; i < tempTasks.length; i++) {
-      console.log(tempTasks[i].id, taskid);
-      if (tempTasks[i].id !== +taskid) {
-        result.push(tempTasks[i]);
-      }
-    }
+    const result = tempTasks.filter(item=> item.id !== taskid)
+    
     setTasks({ tasks: result });
-    StorageService.setItem('tasks',tempTasks)
+    StorageService.setItem('tasks',result)
   }
 
-  const strikeTask = (taskid: number) => {
+  const strikeTask = (taskid: string) => {
     const tempTasks = tasks["tasks"];
     const result = [];
     for (let i = 0; i < tempTasks.length; i++) {
-      if (tempTasks[i].id === +taskid) {
+      if (tempTasks[i].id === taskid) {
         const boolVal= tempTasks[i].strike;
         tempTasks[i].strike = !boolVal
       }
       result.push(tempTasks[i]);
     }
     setTasks({ tasks: result });
-    StorageService.setItem('tasks',tempTasks)
+    StorageService.setItem('tasks',result)
   }
 
   return (
     <div className='App container text-center'>
-      <InputTask addTaskCallback={addTasks} />
-      <ListTasks tasksList={tasks["tasks"]} removeTaskCallback={removeTasks} strikeTaskCallback={strikeTask} />
+      <Header />
+      <AddToDo addTaskCallback={addTasks} />
+      <ToDoList tasksList={tasks["tasks"]} removeTaskCallback={removeTasks} strikeTaskCallback={strikeTask} />
     </div>
   )
 }
