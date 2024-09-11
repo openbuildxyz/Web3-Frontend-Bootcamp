a199ffecc0f8fbec14edc0fb80b5f95b784af16a
Ourai Lin <ourairyu@gmail.com>
2024-09-12 02:59:21 +0800

    fix: Next.js base path

--- .knosys/sites/default/next.config.mjs
+++ .knosys/sites/default/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
+  ...(process.env.NODE_ENV === 'production' ? {
+    basePath: '/Web3-Frontend-Bootcamp',
+    output: 'export',
+  } : undefined),
   reactStrictMode: true,
 };
 
